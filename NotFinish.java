ef06fd12f76a4fa37e1fa29926dad1f6ab9c2e2b
llllIIIIllllIIII <henrywinnie321@yahoo.com.tw>
2016-04-11 08:33:33 +0800

    Create NotFinish.java
    I'm not finish

---
+++ NotFinish.java
@@ -0,0 +1,22 @@
+#import java.util.Scanner;
+#import java.util.ArrayList;
+
+public class BubbleSort{
+	public static void min(String[] args){
+		int chk = 0;
+		
+		ArrayList<Integer> list = new ArrayList<Integer>;
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Please enter the numbers that you want to arrange:")
+		
+		while(chk==0){
+			String input = in.nextLine();
+			if
+			
+		}
+		
+		
+		
+	}
+}
